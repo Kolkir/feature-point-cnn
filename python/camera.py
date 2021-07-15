74f379824c2ed4f9baad83282686705bb5ceb7bd
kolkir <rotate@ukr.net>
2021-07-15 09:15:03 +0300

    Increase FPS for camera

--- python/camera.py
+++ python/camera.py
@@ -1,4 +1,5 @@
 import cv2
+from threading import Thread
 
 
 class Camera(object):
@@ -7,19 +8,32 @@ class Camera(object):
         if not self.cap:
             print('Failed to open camera {0}'.format(index))
             exit(-1)
+
         self.resize_width = img_w
         self.resize_height = img_h
 
+        self.thread = Thread(target=self.update, args=())
+        self.thread.daemon = True
+        self.thread.start()
+        self.status = False
+        self.frame = None
+
+    def update(self):
+        while True:
+            if self.cap.isOpened():
+                (self.status, self.frame) = self.cap.read()
+
     def get_frame(self):
-        ret, frame = self.cap.read()
+        ret, frame = self.status, self.frame
         if ret is False:
             print('Camera failed to capture a frame')
             return None, False
 
         frame = cv2.resize(frame, (self.resize_width, self.resize_height),
                            interpolation=cv2.INTER_AREA)
-        frame = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         frame = frame.astype('float32') / 255.0
+
         return frame, True
 
     def close(self):
