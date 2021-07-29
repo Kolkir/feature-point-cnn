53f017735d41fc6117004040b6671fc4efbefce4
kolkir <rotate@ukr.net>
2021-07-29 10:37:32 +0300

    Minor improvements to coco preprocessing test

--- python/tests/manual/coco-preprocess-test.py
+++ python/tests/manual/coco-preprocess-test.py
@@ -12,7 +12,7 @@ from python.settings import SuperPointSettings
 def draw_points(image, points, color):
     for point in points:
         point_int = (int(round(point[0])), int(round(point[1])))
-        cv2.circle(image, point_int, 3, color, -1, lineType=16)
+        cv2.circle(image, point_int, 2, color, -1, lineType=16)
 
 
 def test_coco_preporcess(coco_path):
@@ -21,7 +21,7 @@ def test_coco_preporcess(coco_path):
     for item in dataset:
         image, point_labels, warped_image, warped_point_labels, valid_mask, homography = item
 
-        show_data('Original', image, point_labels, (0, 255, 0), settings)
+        show_data('Original', image, point_labels, (255, 255, 255), settings)
         show_data('Warped', warped_image, warped_point_labels, (255, 255, 255), settings)
         key = cv2.waitKey(delay=0)
         if key == ord('q'):
@@ -35,7 +35,8 @@ def show_data(name, image, point_labels, color, settings):
     points = points.T
     # Draw result
     original_img = image.permute(1, 2, 0).data.cpu()
-    original_img = cv2.UMat(original_img.numpy())
+    original_img = original_img.numpy()
+    original_img = cv2.UMat(original_img)
     draw_points(original_img, points, color=color)
     cv2.imshow(name, original_img)
 
