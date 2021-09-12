a25980901f23624df1e57ec764e291aa350f20a9
Kirill Kolodiazhnyi <rotate@ukr.net>
2021-09-12 19:59:18 +0300

    Initial implementation of the Axial Attention backbone

--- python/src/superpoint.py
+++ python/src/superpoint.py
@@ -1,44 +1,155 @@
+import math
+
 import torch
 from torch import nn
+import torch.nn.functional as F
+
 from src.netutils import restore_prob_map
 
 
-class ResNetBlock(nn.Module):
-    def __init__(self, in_channels, out_channels, identity_downsample=None, stride=1):
-        super(ResNetBlock, self).__init__()
-        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(out_channels)
-        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=1, stride=1, padding=0, bias=False)
-        self.bn2 = nn.BatchNorm2d(out_channels)
-        self.relu = nn.ReLU()
-        self.identity_downsample = identity_downsample
+def conv1x1(in_planes, out_planes, stride=1):
+    """1x1 convolution"""
+    return nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False)
+
+
+class AxialAttention(nn.Module):
+    def __init__(self, in_planes, out_planes, groups=8, kernel_size=56,
+                 stride=1, bias=False, width=False):
+        assert (in_planes % groups == 0) and (out_planes % groups == 0)
+        super(AxialAttention, self).__init__()
+        self.in_planes = in_planes
+        self.out_planes = out_planes
+        self.groups = groups
+        self.group_planes = out_planes // groups
+        self.kernel_size = kernel_size
+        self.stride = stride
+        self.bias = bias
+        self.width = width
+
+        # Multi-head self attention
+        self.qkv_transform = nn.Conv1d(in_planes, out_planes * 2, kernel_size=1, stride=1,
+                                       padding=0, bias=False)
+        self.bn_qkv = nn.BatchNorm1d(out_planes * 2)
+        self.bn_similarity = nn.BatchNorm2d(groups * 3)
+        self.bn_output = nn.BatchNorm1d(out_planes * 2)
+
+        # Position embedding
+        self.relative = nn.Parameter(torch.randn(self.group_planes * 2, kernel_size * 2 - 1), requires_grad=True)
+        query_index = torch.arange(kernel_size).unsqueeze(0)
+        key_index = torch.arange(kernel_size).unsqueeze(1)
+        relative_index = key_index - query_index + kernel_size - 1
+        self.register_buffer('flatten_index', relative_index.view(-1))
+        if stride > 1:
+            self.pooling = nn.AvgPool2d(stride, stride=stride)
+
+        self.reset_parameters()
+
+    def forward(self, x):
+        if self.width:
+            x = x.permute(0, 2, 1, 3)
+        else:
+            x = x.permute(0, 3, 1, 2)  # N, W, C, H
+        N, W, C, H = x.shape
+        x = x.contiguous().view(N * W, C, H)
+
+        # Transformations
+        qkv = self.bn_qkv(self.qkv_transform(x))
+        q, k, v = torch.split(qkv.reshape(N * W, self.groups, self.group_planes * 2, H),
+                              [self.group_planes // 2, self.group_planes // 2, self.group_planes], dim=2)
+
+        # Calculate position embedding
+        all_embeddings = torch.index_select(self.relative, 1, self.flatten_index).view(self.group_planes * 2,
+                                                                                       self.kernel_size,
+                                                                                       self.kernel_size)
+        q_embedding, k_embedding, v_embedding = torch.split(all_embeddings,
+                                                            [self.group_planes // 2, self.group_planes // 2,
+                                                             self.group_planes], dim=0)
+        qr = torch.einsum('bgci,cij->bgij', q, q_embedding)
+        kr = torch.einsum('bgci,cij->bgij', k, k_embedding).transpose(2, 3)
+        qk = torch.einsum('bgci, bgcj->bgij', q, k)
+        stacked_similarity = torch.cat([qk, qr, kr], dim=1)
+        stacked_similarity = self.bn_similarity(stacked_similarity).view(N * W, 3, self.groups, H, H).sum(dim=1)
+        # (N, groups, H, H, W)
+        similarity = F.softmax(stacked_similarity, dim=3)
+        sv = torch.einsum('bgij,bgcj->bgci', similarity, v)
+        sve = torch.einsum('bgij,cij->bgci', similarity, v_embedding)
+        stacked_output = torch.cat([sv, sve], dim=-1).view(N * W, self.out_planes * 2, H)
+        output = self.bn_output(stacked_output).view(N, W, self.out_planes, 2, H).sum(dim=-2)
+
+        if self.width:
+            output = output.permute(0, 2, 1, 3)
+        else:
+            output = output.permute(0, 2, 3, 1)
+
+        if self.stride > 1:
+            output = self.pooling(output)
+
+        return output
+
+    def reset_parameters(self):
+        self.qkv_transform.weight.data.normal_(0, math.sqrt(1. / self.in_planes))
+        nn.init.normal_(self.relative, 0., math.sqrt(1. / self.group_planes))
+
+
+class AxialBlock(nn.Module):
+    expansion = 2
+
+    def __init__(self, inplanes, planes, stride=1, downsample=None, groups=1,
+                 base_width=64, kernel_size=56):
+        super(AxialBlock, self).__init__()
+        width = int(planes * (base_width / 64.))
+        # Both self.conv2 and self.downsample layers downsample the input when stride != 1
+        self.conv_down = conv1x1(inplanes, width)
+        self.bn1 = nn.BatchNorm2d(width)
+        self.hight_block = AxialAttention(width, width, groups=groups, kernel_size=kernel_size)
+        self.width_block = AxialAttention(width, width, groups=groups, kernel_size=kernel_size, stride=stride,
+                                          width=True)
+        self.conv_up = conv1x1(width, planes * self.expansion)
+        self.bn2 = nn.BatchNorm2d(planes * self.expansion)
+        self.relu = nn.ReLU(inplace=True)
+        self.downsample = downsample
+        self.stride = stride
 
     def forward(self, x):
         identity = x
-        x = self.conv1(x)
-        x = self.bn1(x)
-        x = self.relu(x)
-        x = self.conv2(x)
-        x = self.bn2(x)
 
-        if self.identity_downsample is not None:
-            identity = self.identity_downsample(identity)
+        out = self.conv_down(x)
+        out = self.bn1(out)
+        out = self.relu(out)
 
-        x += identity
-        x = self.relu(x)
-        return x
+        out = self.hight_block(out)
+        out = self.width_block(out)
+        out = self.relu(out)
+
+        out = self.conv_up(out)
+        out = self.bn2(out)
+
+        if self.downsample is not None:
+            identity = self.downsample(x)
+
+        out += identity
+        out = self.relu(out)
+
+        return out
 
 
-def make_layers(num_residual_blocks, in_channels, intermediate_channels, stride):
-    layers = []
+def make_layer(planes, blocks, kernel_size=56, stride=1, inplanes=64, groups=8, base_width=64):
+    downsample = None
+    if stride != 1 or inplanes != planes * AxialBlock.expansion:
+        downsample = nn.Sequential(
+            conv1x1(inplanes, planes * AxialBlock.expansion, stride),
+            nn.BatchNorm2d(planes * AxialBlock.expansion),
+        )
 
-    identity_downsample = nn.Sequential(
-        nn.Conv2d(in_channels, intermediate_channels, kernel_size=1, stride=stride, bias=False),
-        nn.BatchNorm2d(intermediate_channels))
-    layers.append(ResNetBlock(in_channels, intermediate_channels, identity_downsample, stride))
+    layers = [AxialBlock(inplanes, planes, stride, downsample, groups=groups,
+                         base_width=base_width, kernel_size=kernel_size)]
+    inplanes = planes * AxialBlock.expansion
+    if stride != 1:
+        kernel_size = kernel_size // 2
 
-    for i in range(num_residual_blocks - 1):
-        layers.append(ResNetBlock(intermediate_channels, intermediate_channels))
+    for _ in range(1, blocks):
+        layers.append(AxialBlock(inplanes, planes, groups=groups,
+                                 base_width=base_width, kernel_size=kernel_size))
 
     return nn.Sequential(*layers)
 
@@ -47,10 +158,13 @@ class Encoder(nn.Module):
     def __init__(self, image_channels=1):
         super(Encoder, self).__init__()
 
-        self.conv1 = nn.Conv2d(image_channels, 64, kernel_size=7, stride=2, padding=3, bias=False)
-        self.bn1 = nn.BatchNorm2d(64)
-        self.relu = nn.ReLU()
-        self.max_pool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        self.inplanes = 64
+
+        self.conv1 = nn.Conv2d(image_channels, self.inplanes, kernel_size=7, stride=2, padding=3,
+                               bias=False)
+        self.bn1 = nn.BatchNorm2d(self.inplanes)
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
 
     def forward(self, x):
         x = self.conv1(x)
@@ -63,10 +177,10 @@ class Encoder(nn.Module):
 class Detector(nn.Module):
     def __init__(self):
         super(Detector, self).__init__()
-        # ResNetLayers
-        self.layer1 = make_layers(num_residual_blocks=2, in_channels=64, intermediate_channels=64, stride=1)
-        self.layer2 = make_layers(num_residual_blocks=2, in_channels=64, intermediate_channels=128, stride=2)
-        self.out_layer = make_layers(num_residual_blocks=2, in_channels=128, intermediate_channels=65, stride=1)
+
+        self.layer1 = self.make_layer(64, 1, kernel_size=56)
+        self.layer2 = self.make_layer(128, 2, stride=2, kernel_size=56)
+        self.out_layer = self.make_layer(65, 1, stride=2, kernel_size=56)
 
     def forward(self, x):
         x = self.layer1(x)
@@ -78,9 +192,8 @@ class Detector(nn.Module):
 class Descriptor(nn.Module):
     def __init__(self):
         super(Descriptor, self).__init__()
-        # ResNetLayers
-        self.layer1 = make_layers(num_residual_blocks=2, in_channels=64, intermediate_channels=64, stride=1)
-        self.out_layer = make_layers(num_residual_blocks=2, in_channels=64, intermediate_channels=128, stride=2)
+        self.layer1 = self.make_layer(64, 1, kernel_size=56)
+        self.out_layer = self.make_layer(128, 2, stride=2, kernel_size=56)
 
     def forward(self, x):
         x = self.layer1(x)
